a2241bc77825646db1e055775613e27a3d663c08
Bill Schwanitz <bilsch@bilsch.org>
2024-10-28 19:50:46 -0400

    simplify subnets conditional and count

--- main.tf
+++ main.tf
@@ -1,21 +1,4 @@
 locals {
-  # TODO: ipv6 subnets are missing
-  len_public_subnets     = max(length(var.public_subnets), )
-  len_private_subnets    = max(length(var.private_subnets), )
-  len_database_subnets   = max(length(var.database_subnets), )
-  len_kubernetes_subnets = max(length(var.kubernetes_subnets), )
-
-  max_subnet_length = max(
-    local.len_private_subnets,
-    local.len_public_subnets,
-    local.len_database_subnets,
-    local.len_kubernetes_subnets,
-  )
-
-  create_public_subnets     = var.create_public_subnets && local.len_public_subnets > 0
-  create_database_subnets   = var.create_database_subnets && local.len_database_subnets > 0
-  create_private_subnets    = var.create_private_subnets && local.len_private_subnets > 0
-  create_kubernetes_subnets = var.create_kubernetes_subnets && local.len_kubernetes_subnets > 0
 }
 
 resource "azurerm_virtual_network" "this" {
@@ -31,7 +14,7 @@ resource "azurerm_virtual_network" "this" {
 # public subnet resources
 #
 resource "azurerm_subnet" "public" {
-  count = local.create_public_subnets && length(var.public_subnets) > 0 ? 1 : 0
+  count = length(var.public_subnets) > 0 ? 1 : 0
 
   name                 = "public-${count.index}"
   address_prefixes     = var.public_subnets
@@ -43,7 +26,7 @@ resource "azurerm_subnet" "public" {
 # private subnet resources
 #
 resource "azurerm_subnet" "private" {
-  count = local.create_private_subnets && length(var.private_subnets) > 0 ? 1 : 0
+  count = length(var.private_subnets) > 0 ? 1 : 0
 
   name                 = "private-${count.index}"
   address_prefixes     = var.private_subnets
@@ -55,7 +38,7 @@ resource "azurerm_subnet" "private" {
 # database subnet resources
 #
 resource "azurerm_subnet" "database" {
-  count = local.create_database_subnets && length(var.database_subnets) > 0 ? 1 : 0
+  count = length(var.database_subnets) > 0 ? 1 : 0
 
   name                 = "database-${count.index}"
   address_prefixes     = var.database_subnets
@@ -67,7 +50,7 @@ resource "azurerm_subnet" "database" {
 # kubernetes subnet resources
 #
 resource "azurerm_subnet" "kubernetes" {
-  count = local.create_kubernetes_subnets && length(var.kubernetes_subnets) > 0 ? 1 : 0
+  count = length(var.kubernetes_subnets) > 0 ? 1 : 0
 
   name                 = "kubernetes-${count.index}"
   address_prefixes     = var.kubernetes_subnets
